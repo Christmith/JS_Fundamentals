f34b7ea64994b0560e6c06b5fddc2606c1198498
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-14 00:19:51 +0530

    Create fundemental.js

---
+++ fundemental.js
@@ -0,0 +1,8 @@
+//type Hello World in console
+console.log("Hello, World!");
+
+//Declaring variables
+var  height;
+console.log(height);  //  ->  undefined
+console.log(weight);  //  ->  Uncaught  ReferenceError:  weight  is  not  defined
+
