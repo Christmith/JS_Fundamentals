b94be80e3a0775b10db411bc78abbdd1abf4cbd0
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-16 00:00:22 +0530

    Update 07_Operators(II).js

--- 07_Operators(II).js
+++ 07_Operators(II).js
@@ -1,3 +1,129 @@
-
+"use strict"; // using a variable without its previous declaration is treated as an error
 
 /*---------------------------------------String operators------------------------------------------*/
+
+let greetings = "Hi";
+console.log(greetings + " " + "Alice"); // -> Hi Alice
+ 
+let sentence = "Happy New Year ";
+let newSentence = sentence + 10191;
+ 
+console.log(newSentence); // -> Happy New Year 10191
+console.log(typeof newSentence); // -> string
+
+let sentence = "Happy New ";
+sentence += "Year ";
+sentence += 10191;
+console.log(sentence); // -> Happy New Year 10191
+
+/*----------------------------------------- Comparison operators----------------------------------------*/
+
+// Identity (Strict equality) operator ===
+
+console.log(10 === 5); // -> false
+console.log(10 === 10); // -> true
+console.log(10 === 10n); // -> false
+console.log(10 === "10"); // -> false
+console.log("10" === "10"); // -> true
+console.log("Alice" === "Bob"); // -> false
+console.log(0 === false); // -> false
+console.log(undefined === false); // -> false
+
+// Equlaity operator ==
+
+console.log(10 == 5); // -> false
+console.log(10 == 10); // -> true
+console.log(10 == 10n); // -> true
+console.log(10 == "10"); // -> true
+console.log("10" == "10"); // -> true
+console.log("Alice" == "Bob"); // -> false
+console.log(0 == false); // -> true
+console.log(undefined == false); // -> false
+console.log(NaN == NaN); // -> false (if any of the operands has a NaN value (or has been converted to NaN, e.g. with undefined), the equality operator will return false.)
+
+// Nonidentity operator !==
+
+console.log(10 !== 5); // -> true
+console.log(10 !== 10); // -> false
+console.log(10 !== 10n); // -> true
+console.log(10 !== "10"); // -> true
+console.log("10" !== "10"); // -> false
+console.log("Alice" !== "Bob"); // -> true
+console.log(0 !== false); // -> true
+console.log(undefined !== false); // -> true
+
+// inequality operator !=
+
+console.log(10 != 5); // -> true
+console.log(10 != 10); // -> false
+console.log(10 != 10n); // -> false
+console.log(10 != "10"); // -> false
+console.log("10" != "10"); // -> false
+console.log("Alice" != "Bob"); // -> true
+console.log(0 != false); // -> false
+console.log(undefined != false); // -> true
+console.log(NaN != NaN); // -> true
+
+// <,>,<=,>= operators
+
+console.log(10 > 100); // -> false
+console.log(101 > 100); // -> true
+console.log(101 > "100"); // -> true
+ 
+console.log(101 < 100); // -> false
+console.log(100n < 102); // -> true
+console.log("10" < 20n); // -> true
+ 
+console.log(101 <= 100); // -> false
+console.log(10 >= 10n); // -> true
+console.log("10" <=  20); // -> true
+
+console.log("b" > "a"); // -> true
+console.log("a" > "B"); // -> true
+console.log("B" > "A"); // -> true
+console.log("A" > "4"); // -> true
+console.log("4" > "1"); // -> true
+ 
+console.log("ab1" < "ab4"); // -> true
+console.log("ab4" < "abA"); // -> true
+console.log("abB" < "aba"); // -> true
+console.log("aba" < "abb"); // -> true
+ 
+console.log("ab" < "ab4"); // -> true
+
+
+/*---------------------------------------Other Operators----------------------------*/
+
+//  typeof operator
+
+let year = 10191;
+console.log(typeof year); // -> number
+console.log(typeof false); // -> boolean
+
+//  instanceof operator
+
+let names = ["Patti", "Bob"];
+let name = names[0];
+console.log(names instanceof Array); // -> true
+console.log(name instanceof Array); // -> false
+
+// delete operator
+
+let user = {
+     name: "Alice",
+     age: 38
+};
+console.log(user.age); // -> 38
+delete user.age;
+console.log(user.age); // -> undefined
+
+
+// ternary operator
+
+console.log(true ? "Alice" : "Bob"); // -> Alice
+console.log(false ? "Alice" : "Bob"); // -> Bob
+
+let name = 1 > 2 ? "Alice" : "Bob";
+console.log(name); // -> Bob
+
+
