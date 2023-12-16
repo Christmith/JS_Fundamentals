2e6ad0c539dc94287819b52399c34747dd2e2831
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-16 12:28:04 +0530

    Update 08_Interaction_With_Users.js

--- 08_Interaction_With_Users.js
+++ 08_Interaction_With_Users.js
@@ -2,6 +2,14 @@
 
 /*---------------------------------Dialog boxes----------------------------------*/
 // Alert dialog box
+alert("Hello, World!")
+window.alert("Hello, World! for the second time");
+ 
+alert(4 * 7);
+alert(true);
+ 
+alert("text 1", "text 2"); // only "text 1" will be displayed
+
 
 // Confirm dialog box
 
