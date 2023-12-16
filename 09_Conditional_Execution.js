2813956e206c208d29f80979ea2dedf28dc2c950
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-16 16:09:25 +0530

    Update 09_Conditional_Execution.js

--- 09_Conditional_Execution.js
+++ 09_Conditional_Execution.js
@@ -1,1 +1,33 @@
+"use strict"; // using a variable without its previous declaration is treated as an error
+
+/*------------------If statement--------------------*/
+let isUserReady = confirm("Are you ready?");
+console.log(isUserReady);
+if (isUserReady) {
+    alert("User ready!");
+}
+
+let unitPrice = 10;
+let pieces = prompt("How many pieces do you order?", 0);
+if (pieces > 0) {
+    let total = unitPrice * pieces;
+    console.log(total);
+}
+console.log(total); // -> Uncaught ReferenceError: total is not defined
+
+
+let userAge = 23;
+let isFemale = false;
+let points = 703;
+let cartValue = 299;
+let shippingCost = 9.99;
+if (userAge > 21) {
+    if (cartValue >= 300 || points >= 500) {
+        shippingCost = 0;
+    }
+} 
+console.log(shippingCost);
+
+
+/*-------------------------The if ... else statement---------------------------*/
 
