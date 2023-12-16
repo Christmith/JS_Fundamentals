7d9014871dcbd47dab778e14e01d02d8bd6ce797
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-16 12:49:57 +0530

    Update 08_Interaction_With_Users.js

--- 08_Interaction_With_Users.js
+++ 08_Interaction_With_Users.js
@@ -12,6 +12,20 @@ alert("text 1", "text 2"); // only "text 1" will be displayed
 
 
 // Confirm dialog box
+let decision = window.confirm("Is it OK?");
+console.log(decision);
+
+let remove = confirm("Remove all data?");
+let message = remove ? "Deleting Data" : "Cancelled"
+ 
+console.log(message);
+
 
 // Prompt dialog box
+let name = window.prompt("What is your name?", "John Doe");
+name = name ? name : "anonymous";
+ 
+let age = prompt("Hello " + name + " how old are you?");
+alert(name + " is " + age + " years old");
+
 
