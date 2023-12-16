359585fbfd1e0f8ce408f38b003f55bd4a4bab39
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-16 12:24:09 +0530

    Update 08_Interaction_With_Users.js

--- 08_Interaction_With_Users.js
+++ 08_Interaction_With_Users.js
@@ -1,5 +1,9 @@
 "use strict"; // using a variable without its previous declaration is treated as an error
 
 /*---------------------------------Dialog boxes----------------------------------*/
+// Alert dialog box
 
+// Confirm dialog box
+
+// Prompt dialog box
 
