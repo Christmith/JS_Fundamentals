0aaf70dc0cd1962ba698e0bf51647c8aaddc40b6
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-18 12:51:23 +0530

    Update 09_Conditional_Execution.js

--- 09_Conditional_Execution.js
+++ 09_Conditional_Execution.js
@@ -49,3 +49,54 @@ if (isUserReady) {
 
 /*---------------------------------The if … else … if statement------------------------------*/
 
+let number = prompt("Enter a number", 0);
+if (number < 10) {
+    alert("<10");
+} else if (number < 30) {
+    alert("<30");
+} else if (number < 60) {
+    alert("<60");
+} else if (number < 90) {
+    alert("<90");
+} else if (number < 100) {
+    alert("<100");
+} else if (number == 100) {
+    alert("100")
+} else {
+    alert(">100")
+}
+
+/*---------------------------Condition operaor-------------------------------*/
+
+let price = 100;
+let shippingCost = price > 50 ? 0 : 5;
+console.log(`price = ${price}, shipping = ${shippingCost}`); // -> price = 100, shipping = 0
+
+let start = confirm("Start?");
+start ? alert("Here we go!") : console.log("Aborted");
+
+
+
+/*----------------------The switch … case statement-----------------------------*/
+let gate = prompt("Choose gate: a, b, or c");
+let win = false;
+ 
+switch (gate) {
+    case "a":
+        alert("Gate A: empty");
+        break;
+    case "b":
+        alert("Gate B: main prize");
+        win = true;
+        break;
+    case "c":
+        alert("Gate C: empty");
+        break;
+    default:
+        alert("No gate " + String(gate));
+}
+ 
+if (win) {
+    alert("Winner!");
+}
+
