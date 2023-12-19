a67111c686fd0f8e3b31daf952313075413ea09e
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-20 01:21:47 +0530

    Update 10_Loops.js

--- 10_Loops.js
+++ 10_Loops.js
@@ -1,1 +1,27 @@
-1
+"use strict";
+
+/*-------------------------The While Loop---------------------------*/
+
+let n = 0;
+while(n < 91) {
+    console.log(n); // -> 0, 10, 20, 30, 40, 50, 60, 70, 80, 90
+    n += 10;
+}
+
+let isOver = false;
+let counter = 1; 
+while (isOver != true) {
+    let continueLoop = confirm(`[${counter}] Continue the loop?`);
+    isOver = continueLoop === true ? false : true;
+    counter = counter + 1;
+}
+
+
+/*-------------------------The do … while loop---------------------------*/
+
+let isOver;
+let counter = 1; 
+do {
+    isOver = !confirm(`[${counter++}] Continue the loop?`);
+} while (!isOver);
+
