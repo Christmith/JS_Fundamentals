9cfd27bb9139023b78c446910ecdd80bc1b1a4fe
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-20 18:03:55 +0530

    Update 10_Loops.js

--- 10_Loops.js
+++ 10_Loops.js
@@ -100,3 +100,19 @@ for (let city of cities) {
     }
 }
 
+/*------------------------------for … in--------------------------------*/
+
+let user = {
+    name: "Calvin",
+    surname: "Hart",
+    age: 66,
+    email: "CalvinMHart@teleworm.us"
+};
+ 
+for (let key in user) {
+    console.log(key); // -> name, surname, age, email
+};
+
+for (let key in user) {
+        console.log(`${key} -> ${user[key]}`);
+};
