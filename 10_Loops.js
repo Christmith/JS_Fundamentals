930cc31d81b40a4541ccdbb975f062fb596f70b1
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-26 19:38:37 +0530

    Update 10_Loops.js

--- 10_Loops.js
+++ 10_Loops.js
@@ -116,3 +116,20 @@ for (let key in user) {
 for (let key in user) {
         console.log(`${key} -> ${user[key]}`);
 };
+
+
+/*--------------------------The break and continue statements-----------------------*/
+
+// break statement
+
+let i = 0;
+// An infinite loop
+while (true){
+    console.log(i);
+    i++;
+    if (i >= 5) {
+        break;
+    }
+}
+ 
+alert(`Exited the loop with a break (${i}).`);
