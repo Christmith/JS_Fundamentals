600396067b077db6f5fa1d97cc7e4f7379f5ef7d
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-28 21:46:31 +0530

    Create 11_Functions.js

---
+++ 11_Functions.js
@@ -0,0 +1,60 @@
+"use strict"
+
+/*-------------------function declaration---------------------------*/
+
+let temperatures;
+let sum;
+let meanTemp;
+ 
+function getMeanTemp() {
+     sum = 0;
+     for (let i = 0; i < temperatures.length; i++) {
+         sum += temperatures[i];
+     }
+     meanTemp = sum / temperatures.length;    
+}
+
+/*------------------calling function---------------------------*/
+
+let temperatures;
+let sum;
+let meanTemp;
+ 
+function getMeanTemp() {
+     sum = 0;
+     for (let i = 0; i < temperatures.length; i++) {
+      sum += temperatures[i];
+     }
+     meanTemp = sum / temperatures.length; 
+}
+ 
+temperatures = [12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21, 20, 19, 17, 16];
+getMeanTemp();
+console.log(`mean: ${meanTemp}`); // -> mean: 16.666666666666668
+ 
+temperatures = [17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24, 21, 19, 18, 17, 16];
+getMeanTemp();
+console.log(`mean: ${meanTemp}`); // -> mean: 18.083333333333332
+
+
+/*-------------------------function local variables-----------------------------*/
+
+
+let temperatures;
+let meanTemp;
+ 
+function getMeanTemp() {
+     let sum = 0; //This is a local variable. We can use let or var keywords to declare local variables inside the function block
+     for (let i = 0; i < temperatures.length; i++) {
+       sum += temperatures[i];
+     }
+     meanTemp = sum / temperatures.length;
+}
+ 
+temperatures = [12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21, 20, 19, 17, 16];
+getMeanTemp();
+console.log(`mean: ${meanTemp}`); // -> mean: 16.666666666666668
+ 
+temperatures = [17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24, 21, 19, 18, 17, 16];
+getMeanTemp();
+console.log(`mean: ${meanTemp}`); // -> mean: 18.083333333333332
