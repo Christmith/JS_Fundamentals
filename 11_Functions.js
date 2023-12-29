6e763fa5ab5e3aa6eb9d66b585ef53eb23d18c1c
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2023-12-30 00:04:07 +0530

    Update 11_Functions.js

--- 11_Functions.js
+++ 11_Functions.js
@@ -58,3 +58,47 @@ console.log(`mean: ${meanTemp}`); // -> mean: 16.666666666666668
 temperatures = [17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24, 21, 19, 18, 17, 16];
 getMeanTemp();
 console.log(`mean: ${meanTemp}`); // -> mean: 18.083333333333332
+
+
+/*------------------------------return statements--------------------------------*/
+
+let temperatures;
+let meanTemp;
+ 
+function getMeanTemp() {
+     let sum = 0;
+     let result;
+     for (let i = 0; i < temperatures.length; i++) {
+      sum += temperatures[i];
+     }
+     result = sum / temperatures.length;
+     return result;  
+}
+ 
+temperatures = [12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21, 20, 19, 17, 16];
+meanTemp = getMeanTemp();
+console.log(`mean: ${meanTemp}`);
+ 
+temperatures = [17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24, 21, 19, 18, 17, 16];
+meanTemp = getMeanTemp();
+console.log(`mean: ${meanTemp}`);
+
+
+/*----------------------------Functions with parameters----------------------------*/
+
+function getMeanTemp(temperatures) {
+     let sum = 0;
+     for (let i = 0; i < temperatures.length; i++) {
+      sum += temperatures[i];
+     }
+     return sum / temperatures.length;
+}
+ 
+let day1 = [12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21, 20, 19, 17, 16];
+console.log(`mean: ${getMeanTemp(day1)}`); // -> mean:
+16.666666666666668
+ 
+let day2 = [17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24, 21, 19, 18, 17, 16];
+console.log(`mean: ${getMeanTemp(day2)}`); // -> mean:
+18.083333333333332
+
