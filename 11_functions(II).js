3654a2600f38cd056204009e2edf5be343a700ed
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-01-02 10:21:01 +0530

    Update 11_functions(II).js

--- 11_functions(II).js
+++ 11_functions(II).js
@@ -130,3 +130,26 @@ console.log('test 2');
             test 2  */
 
 
+//  Asynchronous callbacks
+
+let inner = function() {
+console.log('inner 1');
+}
+ 
+let outer = function(callback) {
+console.log('outer 1');
+setTimeout(callback, 1000) /*ms*/;
+console.log('outer 2');
+}
+ 
+console.log('test 1');
+outer(inner);
+console.log('test 2');
+
+/* output : test 1
+            outer 1
+            outer 2
+            test 2 
+            ...
+            inner 1
+            */
