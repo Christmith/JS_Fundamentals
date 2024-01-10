81f80dfeea53b8261432bc4580f9e668a2047b14
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-01-10 21:21:54 +0530

    Update 13_Errors&ExceptionHandling.js

--- 13_Errors&ExceptionHandling.js
+++ 13_Errors&ExceptionHandling.js
@@ -1,22 +1,35 @@
-/*----------------------------Syntax Error-----------------------------*/
+/*----------------------------------------------Type of errors-------------------------------------------*/
 
-"use strict";
+/*----------------------------Syntax Error-------------------------------*/
+
 iff (true) { //-> Uncaught SyntaxError: Unexpected token '{'
     console.log("true");
 }
-
-
 /*---------------------------Reference Error-----------------------------*/
 
 let a = b; // -> Uncaught ReferenceError: b is not defined
 fun(); // -> Uncaught ReferenceError: fun is not defined
 
-
-
-/*---------------------------Type Error------------------------------*/
+/*---------------------------Type Error---------------------------------*/
 
 const someConstValue = 5;
 someConstValue = 7; // -> Uncaught TypeError: Assignment to constant variable.
-
 let someNumber = 10;
 someNumber.length(); // -> Uncaught TypeError: someNumber.length is not a function
+
+/*--------------------------Range Error---------------------------------*/
+
+let testArray1 = Array(10);
+console.log(testArray1.length); // -> 10
+let testArray2 = Array(-1); // -> Uncaught RangeError: Invalid array length
+console.log(testArray2.length);
+
+/*---------------------------------------------The try...catch statement-----------------------------------*/
+
+try {
+    let a = b;
+} catch (error) {
+    console.log("Caught " + error); // -> Caught ReferenceError: b is not defined
+}
+console.log("We handled the exception!"); // -> we handled the exception!
+
