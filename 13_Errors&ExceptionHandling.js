09b645be7e06e82ea9a27c34962c83cd14738522
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-01-10 12:30:13 +0530

    Update 13_Errors&ExceptionHandling.js

--- 13_Errors&ExceptionHandling.js
+++ 13_Errors&ExceptionHandling.js
@@ -1,1 +1,9 @@
+/*--------------------------------------------exception handling-----------------------------------------------------*/
+
+try {
+    console.log('abc'); // -> abc
+    conole.log('abc');
+} catch (error) {  
+    console.log(error.message); // -> conole is not defined 
+}
 
