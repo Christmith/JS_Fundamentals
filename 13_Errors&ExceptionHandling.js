ac21ff3939672982e9f4d308489a0156ef6a8b91
Yohan Christmith Weerapperuma <127888824+Christmith@users.noreply.github.com>
2024-01-10 13:47:54 +0530

    Update 13_Errors&ExceptionHandling.js

--- 13_Errors&ExceptionHandling.js
+++ 13_Errors&ExceptionHandling.js
@@ -1,9 +1,22 @@
-/*--------------------------------------------exception handling-----------------------------------------------------*/
+/*----------------------------Syntax Error-----------------------------*/
 
-try {
-    console.log('abc'); // -> abc
-    conole.log('abc');
-} catch (error) {  
-    console.log(error.message); // -> conole is not defined 
+"use strict";
+iff (true) { //-> Uncaught SyntaxError: Unexpected token '{'
+    console.log("true");
 }
 
+
+/*---------------------------Reference Error-----------------------------*/
+
+let a = b; // -> Uncaught ReferenceError: b is not defined
+fun(); // -> Uncaught ReferenceError: fun is not defined
+
+
+
+/*---------------------------Type Error------------------------------*/
+
+const someConstValue = 5;
+someConstValue = 7; // -> Uncaught TypeError: Assignment to constant variable.
+
+let someNumber = 10;
+someNumber.length(); // -> Uncaught TypeError: someNumber.length is not a function
